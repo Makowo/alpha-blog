e7fe389f0ff6e3c3d3b5e5ab3c10e243b8044978
Jonathan Hansen-Blum <jonhansenblum@gmail.com>
2022-09-15 23:26:50 -0500

    Add admin requirement to create new categories.

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "potato@potato.com",
+                              password: "true", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "Get new category form and create category" do
     get "/categories/new"
     assert_response :success
